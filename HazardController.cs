06bbf7981d26816893bf07b44daaa2e47f891345
Enderclaw the Cyborg <epicenderman226@gmail.com>
2021-09-28 11:16:07 -0700

    Add files via upload

---
+++ HazardController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HazardController : MonoBehaviour
+{
+    public Text failText;
+
+
+    void Start()
+    {
+        print("Hazard is active");
+        failText.text = "";
+    }
+
+    
+    void OnTriggerEnter2D(Collider2D Hazard)
+    {
+        if(Hazard.gameObject.CompareTag("Hazard"))
+        {
+            Hazard.gameObject.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            SetFailText();
+        }
+    }
+
+    void SetFailText()
+    {
+        failText.text = "Oops! Not that pickup! Try again";
+    }
+}
